7aea3f15d74e8644d75475670a488bd590f43c7f
waotzi <waotzi@proton.me>
2023-03-05 23:08:57 +0700

    new attempt to get the webhook secret

--- update_webhook_conf.sh
+++ update_webhook_conf.sh
@@ -14,7 +14,7 @@ cp "${HOOKS_FILE_PATH}" "${WEBHOOK_CONF_PATH}"
 
 
 # Get the value of WEBHOOK_KEY from environment variables in .env file
-WEBHOOK_KEY=$(grep 'WEBHOOK_KEY' "${ENV_FILE_PATH}" | cut -d '=' -f 2)
+WEBHOOK_SECRET=$(grep '^WEBHOOK_SECRET=' "${ENV_FILE_PATH}" | cut -d '=' -f 2)
 
 # Replace mysecr3tC0d3 with WEBHOOK_KEY in hooks.yml
-sed -i "s/mysecr3tC0d3/${WEBHOOK_KEY}/g" "${WEBHOOK_CONF_PATH}"
+sed -i "s/mysecr3tC0d3/${WEBHOOK_SECRET}/g" "${WEBHOOK_CONF_PATH}"
